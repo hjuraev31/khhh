9d5fb7f4edc1cb4394213a7de66eb388ecba0bd6
hjuraev31 <hjuraev31@gmail.com>
2022-02-21 19:34:34 +0500

    api update

--- events/views.py
+++ events/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Events
 from .serializers import EventsSerializer
-class EventList(generics.ListAPIView):
+class EventList(generics.ListCreateAPIView):
 	queryset = Events.objects.all()
 	serializer_class = EventsSerializer
 
-class DetailEvent(generics.RetrieveAPIView):
+class DetailEvent(generics.RetrieveUpdateDestroyAPIView):
 	queryset = Events.objects.all()
 	serializer_class = EventsSerializer
