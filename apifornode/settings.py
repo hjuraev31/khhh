ecd8bcf6a483986ecbba3238d38938af7cb2930b
hjuraev31 <hjuraev31@gmail.com>
2022-02-23 22:44:35 +0500

    clickjacking disable

--- apifornode/settings.py
+++ apifornode/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = (
