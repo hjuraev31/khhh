862e822282f557389925ec11418096f641372325
Khusniddin <52568371+hjuraev31@users.noreply.github.com>
2022-06-26 16:45:38 +0500

    Update settings.py

--- apifornode/settings.py
+++ apifornode/settings.py
@@ -65,11 +65,13 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
     'https://lms.tuit.uz',
     'https://jahanbaev.github.io/portfolio/admin',
+    'https://jahanbaev-admin.github.io',
     'https://github.com',
 )
 CORS_ALLOWED_ORIGINS = [
     'https://lms.tuit.uz',
     'https://jahanbaev.github.io',
+    'https://jahanbaev-admin.github.io',
     'https://github.com',
 ]
 
