385d38c623c68df8c43b4bc09248ce48bca43326
Khusniddin <52568371+hjuraev31@users.noreply.github.com>
2022-07-01 19:36:57 +0500

    Update models.py

--- events/models.py
+++ events/models.py
@@ -4,5 +4,5 @@ class Events(models.Model):
 	student_id = models.TextField(default='')
 	text = models.TextField(default='')
 	def __str__(self):
-		return str(self.student_id)
+		return str(self.text)
 # Create your models here.
