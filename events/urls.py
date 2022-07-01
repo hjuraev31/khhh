2b7e86290a4050d654ee7684c5062fd7834ac188
Khusniddin <52568371+hjuraev31@users.noreply.github.com>
2022-07-01 19:59:21 +0500

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import EventList, DetailEvent, bodyText, byName
+from .views import EventList, DetailEvent, DeleteDataBase, bodyText, byName
 
 urlpatterns = [
 	path('name/<str:name>/', byName, name='FilterByName'),
 	path('text/<int:pk>/', bodyText, name='bodyText'),
 	path('<int:pk>/', DetailEvent.as_view(), name='detailevent'),
+	path('deleteDataBase/',DeleteDataBase.as_view(), name='deleteDB'),
 	path('', EventList.as_view(), name='eventlist'),
 ]
