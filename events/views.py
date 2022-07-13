dafc2038fe49dee07669b6ea25f83bb4561fc16a
Khusniddin <hjuraev31@gmail.com>
2022-07-13 22:43:40 +0500

    imgdb

--- events/views.py
+++ events/views.py
@@ -29,8 +29,8 @@ class ImgList(generics.ListCreateAPIView):
 
 class ImageAPIView(generics.RetrieveAPIView):
 
-    queryset = ImgDB.objects.get(id = 1)
-    renderer_classes = [JPEGRenderer]
+    # queryset = ImgDB.objects.get(id = int(id))
+    # renderer_classes = [JPEGRenderer]
 
     def get(self, request, *args, **kwargs):
         renderer_classes = [JPEGRenderer]
